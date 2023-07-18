8bcb3036aacd91cc93f1d789b937b215d87c3ce9
dcdcdcdc221 <909035461@qq.com>
2023-07-18 10:46:21 +0800

    CRUD first taste

---
+++ src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author admin
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2023-07-11 10:56:36
+* @Entity com.yupi.springbootinit.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
